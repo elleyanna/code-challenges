296deaedd2559621011e08ec107ccdb99b03b6bb
Yanna Dwyer <yanna@rebeccas-mbp.myfiosgateway.com>
2025-03-05 01:43:39 -0500

    Clean up node modules and styling.

--- septa-fare-calculator/src/components/FareCalculator/index.tsx
+++ septa-fare-calculator/src/components/FareCalculator/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FareData } from "../../types/fare";
+import "./fareCalculator.scss";
 
 const FareCalculator = () => {
   const [zone, setZone] = useState(1);
@@ -17,105 +18,135 @@ const FareCalculator = () => {
         if (!response.ok) throw new Error();
         const data = await response.json();
         setFareData(data);
-        console.log("fare data: ", fareData);
       } catch (error) {
-        setError(error);
-        console.log("error ", error);
+        setError("Failed to fetch fare data");
+        console.error("error ", error);
       }
     };
     fetchData();
   }, []);
 
-  const handleSubmit = (e: { preventDefault: () => void }) => {
-    e.preventDefault();
+  useEffect(() => {
+    if (!fareData) return;
 
-    const selectedZone = fareData?.zones.find(
-      (item: { zone: number }) => item.zone === Number(zone)
+    const selectedZone = fareData.zones.find(
+      (item) => item.zone === Number(zone)
     );
-    if (!selectedZone) return;
+    if (!selectedZone) {
+      setTotalPrice(null);
+      return;
+    }
 
     const fareOption = selectedZone.fares.find(
-      (option: { type: string; purchase: string }) =>
-        option.type === type && option.purchase === purchase
+      (option) => option.type === type && option.purchase === purchase
     );
-    if (!fareOption) return;
+    if (!fareOption) {
+      setTotalPrice(null);
+      return;
+    }
 
     const pricePerTrip = fareOption.price / fareOption.trips;
     const calculatedTotal = pricePerTrip * trips;
 
     setTotalPrice(calculatedTotal.toFixed(2));
+  }, [zone, type, purchase, trips, fareData]);
+
+  const getFareHelperText = () => {
+    switch (type) {
+      case "anytime":
+        return fareData?.info?.anytime;
+      case "weekday":
+        return fareData?.info?.weekday;
+      case "evening_weekend":
+        return fareData?.info.evening_weekend;
+      default:
+        return "";
+    }
   };
 
   return (
-    <div>
-      <h2>SEPTA Fare Calculator</h2>
-      {type === "anytime" && <p>{fareData?.info.anytime}</p>}
-      <form onSubmit={handleSubmit}>
-        <label>
-          Zone:
-          <select
-            value={zone}
-            onChange={(e) => setZone(Number(e.target.value))}
-          >
-            {fareData?.zones.map((item) => (
-              <option key={item.zone} value={item.zone}>
-                {item.name}
-              </option>
-            ))}
-          </select>
-        </label>
-        <label>
-          Type:
-          <select
-            value={type}
-            onChange={(e: { target: { value: string } }) =>
-              setType(e.target.value)
-            }
-          >
-            <option value="weekday">Weekday</option>
-            <option value="evening_weekend">Evening/Weekend</option>
-            <option value="anytime">Anytime</option>
-          </select>
-        </label>
-        <fieldset>
-          <legend>Purchase Method:</legend>
-          <label>
-            <input
-              type="radio"
-              value="advance_purchase"
-              checked={purchase === "advance_purchase"}
-              onChange={(e: { target: { value: string } }) =>
-                setPurchase(e.target.value)
-              }
-            />
-            Advance Purchase
+    <div className="fare-calculator">
+      <div className="heading-section">
+        <img src="/septa.png" alt="Septa logo" className="septa-logo" />
+        <h1 className="heading-text">Regional Rail Fares</h1>
+      </div>
+      <form>
+        <div className="section">
+          <label className="section-header">
+            Where are you going?
+            <select
+              className="dropdown-select"
+              value={zone}
+              onChange={(e) => setZone(Number(e.target.value))}
+            >
+              {fareData?.zones.map((item) => (
+                <option key={item.zone} value={item.zone}>
+                  {item.name}
+                </option>
+              ))}
+            </select>
           </label>
-          <label>
-            <input
-              type="radio"
-              value="onboard_purchase"
-              checked={purchase === "onboard_purchase"}
-              onChange={(e: { target: { value: string } }) =>
-                setPurchase(e.target.value)
-              }
-            />
-            Onboard Purchase
+        </div>
+        <div className="section">
+          <label className="section-header">
+            When are you riding?
+            <select
+              className="dropdown-select"
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+            >
+              <option value="weekday">Weekdays</option>
+              <option value="evening_weekend">Evening/Weekend</option>
+              <option value="anytime">Anytime</option>
+            </select>
           </label>
-        </fieldset>
-        <label>
-          Trips:
-          <input
-            type="number"
-            value={trips}
-            onChange={(e) => setTrips(Number(e.target.value))}
-            min="1"
-          />
-        </label>
-        <button type="submit">Calculate Fare</button>
+          <p className="fare-data">{getFareHelperText()}</p>
+        </div>
+        <div className="section fare">
+          <fieldset className="radio-buttons">
+            <legend className="section-header">
+              Where will you purchase the fare?
+            </legend>
+            <label className="radio-button">
+              <input
+                type="radio"
+                value="advance_purchase"
+                checked={purchase === "advance_purchase"}
+                onChange={(e) => setPurchase(e.target.value)}
+              />
+              Station Kiosk
+            </label>
+            <label className="radio-button">
+              <input
+                type="radio"
+                value="onboard_purchase"
+                checked={purchase === "onboard_purchase"}
+                onChange={(e) => setPurchase(e.target.value)}
+              />
+              Onboard
+            </label>
+          </fieldset>
+        </div>
+        <div className="section trips">
+          <label className="section-header">
+            How many rides will you need?
+            <input
+              type="number"
+              className="trip-input"
+              value={trips}
+              onChange={(e) => setTrips(Number(e.target.value))}
+              min="1"
+            />
+          </label>
+        </div>
       </form>
-
-      {totalPrice && <div>Total Price: ${totalPrice}</div>}
-      {error && <p>{error}</p>}
+      {totalPrice && (
+        <div className="total-price">
+          <p>Your fare will cost:</p>
+          <h2>${totalPrice}</h2>
+        </div>
+      )}
+      {error && <p className="error">{error}</p>}
     </div>
   );
 };
